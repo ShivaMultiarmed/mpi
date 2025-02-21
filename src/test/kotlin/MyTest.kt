7fc85366c251e6a6793b1ecb3be0587470b0723e
Mikhail_Shell <theoutstandingme@gmail.com>
2025-02-21 23:44:44 +0700

    readded results from the 1 lab

--- src/test/kotlin/MyTest.kt
+++ src/test/kotlin/MyTest.kt
@@ -1,2 +1,18 @@
+import org.junit.jupiter.api.Test
+import java.io.FileInputStream
+import java.io.FileOutputStream
+import java.io.PrintWriter
+
+
 class MyTest {
+    @Test
+    fun replaceColons() {
+        PrintWriter(FileOutputStream("accompanying_folder/lab1/task2/results_copy.txt")).use { writer ->
+            FileInputStream("accompanying_folder/lab1/task2/results.txt").bufferedReader().use { reader ->
+                reader.forEachLine {
+                    writer.println(it.replace(": ", "\t"))
+                }
+            }
+        }
+    }
 }
